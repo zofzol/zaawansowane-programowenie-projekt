d0a029b8f5f2979c22c493cc507d9d4b7218f604
zofzol <zosia.zoladkioewicz@gmail.com>
2026-03-29 14:32:54 +0200

    Dodaj pliki projektów.

---
+++ zaawansowane programowenie projekt/Form1.cs
@@ -0,0 +1,10 @@
+namespace zaawansowane_programowenie_projekt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
